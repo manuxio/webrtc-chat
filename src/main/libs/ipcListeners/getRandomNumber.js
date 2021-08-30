972863a722f4be9e31f518fd3c620519c9ba36c5
Manuele <manu@cappelleri.net>
2021-08-30 18:14:09 +0200

    OK

--- src/main/libs/ipcListeners/getRandomNumber.js
+++ src/main/libs/ipcListeners/getRandomNumber.js
@@ -6,7 +6,7 @@ export default (/* app, appState, appConfig */) => {
     console.log('Get Random Number Request');
     // console.log('Posting to', `${appConfig.roomsApiServer}/user/authenticate`, username, password);
     const reply = new JsonReply(Math.random(10));
-    return Promise.resolve(reply);
+    return Promise.resolve(reply.toJSON());
 
 
   });
